20ff65f271f01589818c3647d9c9d8a244ff929b
DeArtanyan <stretovich200@gmail.com>
2025-02-05 04:56:14 +0700

    first 1 commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let app = new Vue ({
+    el: '#app',
+    data:{
+        product:"Socks",
+        description: "A pair of warm, fussy socks",
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        onSale: true,
+    }
+})
